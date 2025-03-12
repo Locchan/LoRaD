64bcc62f172524ecc0594d3685135f20c0f09b94
Jaroslaw Locchansky <locchansky@gmail.com>
2025-03-12 17:11:55 +0300

    Change date format in getver

--- getver.sh
+++ getver.sh
@@ -1,4 +1,4 @@
-LAST_COMMIT_DATE=$(git log -1 --date=format:"%Y/%m/%d %T" --format="%ad")
+LAST_COMMIT_DATE=$(git log -1 --date=format:"%d.%m.%Y %T" --format="%ad")
 LAST_COMMIT_HASH=$(git log --pretty=format:'%h' -n 1)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 LAST_COMMIT_NUMBER=$(git rev-list --count $CURRENT_BRANCH)
