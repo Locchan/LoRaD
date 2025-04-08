36c919dc0946718c88671e3a45ae68264a5ec948
Сергей Титов <s.tsitou@launchoptions.by>
2025-04-08 13:48:32 +0300

    Update getver helper script

--- getver.sh
+++ getver.sh
@@ -4,4 +4,16 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 LAST_COMMIT_NUMBER=$(git rev-list --count $CURRENT_BRANCH)
 LORAD_VERSION=$(sed -nE 's/version = "(.*)"/\1/p' pyproject.toml)
 
+while test $# -gt 0
+do
+    case "$1" in
+        --short) echo "$LORAD_VERSION.$LAST_COMMIT_NUMBER";
+        exit 0
+            ;;
+    esac
+    shift
+done
+
 echo "LoRaD v.$LORAD_VERSION.$LAST_COMMIT_NUMBER (Commit: $LAST_COMMIT_HASH; $LAST_COMMIT_DATE)"
+
+exit 0
