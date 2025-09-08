76bc53ecc651aa295f7448a0682e4bd37d1c0dca
Jaroslaw Locchansky <locchansky@gmail.com>
2025-09-08 13:01:27 +0300

    Migrate upgrade install to script installation method.

--- upgrade_install.sh
+++ upgrade_install.sh
@@ -4,12 +4,13 @@ set -e
 set -x
 
 apk add ffmpeg
-pip install --no-cache-dir poetry
+pip install poetry
 poetry build
-pip install --no-cache-dir dist/*.whl
+pip install --upgrade --no-deps --force-reinstall dist/*.whl
+pip install --upgrade dist/*.whl
 cp /tmp/lorad/lorad_main.py /usr/bin/lorad
 chmod +x /usr/bin/lorad
 rm -rf /tmp/lorad
-ln -s /usr/share/zoneinfo/Europe/Minsk /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Minsk /etc/localtime
 echo "${VERSION}" > /version
 pip freeze > /packages
