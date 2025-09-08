69ed279962c501f3f4374a74460dd1cd3fe5b920
Jaroslaw Locchansky <locchansky@gmail.com>
2025-09-08 12:51:07 +0300

    Build with a bash script, not load of RUN in Dockerfile

---
+++ full_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+set -x
+
+apk add ffmpeg
+pip install --no-cache-dir poetry
+poetry build
+pip install --no-cache-dir dist/*.whl
+cp /tmp/lorad/lorad_main.py /usr/bin/lorad
+chmod +x /usr/bin/lorad
+cp -r /tmp/lorad/frontend/dist/lorad-frontend/browser/* /app/ui/
+rm -rf /tmp/lorad
+ln -s /usr/share/zoneinfo/Europe/Minsk /etc/localtime
+echo "${VERSION}" > /version
+pip freeze > /packages
