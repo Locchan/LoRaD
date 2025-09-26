93a6556d7c4da673ff0bf842a6aa32227e004bb9
Jaroslaw Locchansky <locchansky@gmail.com>
2025-09-26 22:58:54 +0300

    Set environment for prod

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://lorad:5476', // REST API port from the Python backend
-  autoplay: true, // Default to true, can be overridden by Docker environment variable
-  radioTitle: 'ЙОПТЫМОПТЫ', // Radio station title
+  apiUrl: 'http://lorad:5476',
+  autoplay: false,
+  radioTitle: 'Дедорадио',
 };
