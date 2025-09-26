87ea8e425aa54b97b89bd9f36c2185b17344db84
Jaroslaw Locchansky <locchansky@gmail.com>
2025-09-27 01:19:48 +0300

    Allow adding random files to the news

--- lorad/audio/programs/NewsPrgS.py
+++ lorad/audio/programs/NewsPrgS.py
@@ -8,7 +8,7 @@ from lorad.audio.programs.news.orm import News
 from lorad.audio.programs.news.neuro.neurovoice import check_voiced, get_filelist, voice_news
 from lorad.audio.utils.ffmpeg_utils import ffmpeg_concatenate, ffmpeg_reencode
 from lorad.common.database.MySQL import MySQL
-from lorad.common.utils.globs import FEAT_FAKE_NEWS, FEAT_ADS
+from lorad.common.utils.globs import FEAT_FAKE_NEWS, FEAT_NEWS_ADS, FEAT_NEWS_RANDOM_FILE
 from lorad.common.utils.logger import get_logger
 from lorad.common.utils.misc import read_config, feature_enabled
 
@@ -101,17 +101,32 @@ class NewsPrgS(GenericPrg):
             if not os.path.exists(tmpfilename):
                 ffmpeg_reencode(afile, ["-b:a", f"{self.config["BITRATE_KBPS"]}k", "-c:a", "libmp3lame", "-ar", "44100", "-ac", "2", "-af", "apad=pad_dur=2"], tmpfilename)
         logger.info("Concatenating news files into a digest")
-        if feature_enabled(FEAT_ADS):
+        if feature_enabled(FEAT_NEWS_ADS):
             temp_files = self.add_ads(tempfiles)
+        if feature_enabled(FEAT_NEWS_RANDOM_FILE):
+            temp_files = self.add_random_files(tempfiles)
         logger.debug("Will use the following files:")
         logger.debug(temp_files)
         ffmpeg_concatenate(tempfiles, news_file, artist="NeuroNews", title=f"Новости за {datetime.datetime.now().strftime("%Y-%m-%d %H")}")
         self._cleanup()
         return news_file
-    
+
+    def add_random_files(self, files_list, count=1):
+        logger.info(f"Adding {count} random files to the news")
+        random_filesdir = os.path.join(self.config["DATADIR"], "resources", "random_voices")
+        ad_files = [os.path.join(random_filesdir, f) for f in os.listdir(random_filesdir) if os.path.isfile(os.path.join(random_filesdir, f))]
+        if not ad_files:
+            logger.warning("Could not get a random file to add to the news.")
+            return files_list
+        random_files_to_add = random.sample(ad_files, count)
+        if not isinstance(random_files_to_add, list):
+            random_files_to_add = [random_files_to_add]
+        files_list.extend(random_files_to_add)
+        return files_list
+
     def add_ads(self, files_list, count=1):
         logger.info(f"Adding {count} ads to the news")
-        adsdir = os.path.join(self.config["DATADIR"], "resources", "ads")
+        adsdir = os.path.join(self.config["DATADIR"], "resources", "random_voices")
         ad_files = [os.path.join(adsdir, f) for f in os.listdir(adsdir) if os.path.isfile(os.path.join(adsdir, f))]
         if not ad_files:
             logger.warning("Could not get an ad to add to the news.")
@@ -121,7 +136,7 @@ class NewsPrgS(GenericPrg):
             ads_to_add = [ads_to_add]
         files_list.extend(ads_to_add)
         return files_list
-    
+
     def _cleanup(self):
         logger.info("Starting voice file cleanup...")
         newsdir = os.path.join(self.config["DATADIR"], "neurovoice")
